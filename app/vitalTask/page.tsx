89ab92e2c69fabef1703bd95d8d762addc3f52a2
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-07-25 23:12:06 +0400

    feat : add new page

---
+++ app/vitalTask/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.scss";
+
+export default function VitalTasks() {
+  return (
+    <section className={styles.contianer}>
+      <div className={styles.VitalTasks}></div>
+      <div className={styles.VitalTask}></div>
+    </section>
+  );
+}
