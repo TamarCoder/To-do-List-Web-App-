0fa0fe2617820465ce83714f98ddeab01effcfbd
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-07-26 10:47:17 +0400

    fix: node js

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -11,7 +11,7 @@ export const Navigation = () => {
 
   const navItems = [
     { href: "/home", name: IconName.dashboared, label: "Dashboard" },
-    { href: "/vitalTask", name: IconName.vital, label: "Vital Task" },
+    { href: "#", name: IconName.vital, label: "Vital Task" },
     { href: "#", name: IconName.myTask, label: "My Tasks" },
     { href: "#", name: IconName.tasks, label: "Task Categories" },
     { href: "#", name: IconName.setting, label: "Settings" },
